94a364671023c59b2a239fa9b3a3e93f6467007d
AkshayGit17 <uakshaykini@gmail.com>
2022-02-24 13:39:48 +0530

    register functionality and JWT setup

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,28 @@
+const { StatusCodes } = require('http-status-codes');
+
+const User = require('../models/User');
+const { BadRequestError } = require('../errors');
+const { createJWT } = require('../utils');
+
 const register = async (req, res) => {
-  res.send('register');
+  const { name, email, password } = req.body;
+
+  const emailAlreadyExists = await User.findOne({ email });
+
+  if (emailAlreadyExists) {
+    throw new BadRequestError('Email already exists');
+  }
+
+  const isFirstAccount = (await User.countDocuments({})) === 0;
+  const role = isFirstAccount ? 'admin' : 'user';
+
+  const user = await User.create({ name, email, password, role });
+
+  const tokenUser = { name: user.name, id: user._id, role: user.role };
+
+  const token = createJWT({ payload: tokenUser });
+
+  res.status(StatusCodes.CREATED).json({ user: tokenUser, token });
 };
 const login = async (req, res) => {
   res.send('login');
